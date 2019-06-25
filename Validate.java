fbb3594e0d4894a1e0615b702d310337b4a84da7
PeterKaniu <peterkaniu254@gmail.com>
2019-06-25 12:45:58 +0300

    Update Validate.java
    Method "operatorSeq" committed to ensures that two subsequent operators are valid and in the correct order

--- Validate.java
+++ Validate.java
@@ -1,3 +1,43 @@
+package bodmas;
+
+
+
 public class Validates {
 
+	//This method ensures that two subsequent operators are valid and in the correct order
+  public static boolean operatorSeq(String expression)
+    {
+
+        char[] tokens = expression.toCharArray();
+
+        // Stack for numbers: 'values'
+        for (int i = 0; i < tokens.length; i++) {
+        		
+        		if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/'  || tokens[i] == '(' || tokens[i] == ')'  || tokens[i] == '&') {
+        			
+        			// check when user enters similar subsequent operators
+        			if(tokens[i] ==  tokens[i+1] ) {
+						tokens[i] = ' ';
+						i--;
+    				}
+        			
+        			if(tokens[i+1] == '+' || tokens[i+1] == '-' || tokens[i + 1] == '(' ){
+        				if(tokens[i+1] == '+' || tokens[i+1] == '-' ) {// when use enters + and - subsequently, remove + from equation and retain -
+        					if(tokens[i] == '-' || tokens[i+1] == '-') {
+        						tokens[i] = ' ';
+        						tokens[i+1] = '-';
+            					
+            				}
+        				}
+        				
+        
+        				return true;//return true confirming correct order of operators
+        			}else if(tokens[i+1] == '*' || tokens[i+1] == '/'  || tokens[i+1] == ')'  || tokens[i+1] == '&') {
+        				return false;//return false confirming wrong order of subsequent operators
+    				}
+        		}
+        		}
+		return true;
+
+        }
 }
